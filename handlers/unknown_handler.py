cfd162f38d4bd532dcddcaace80a68b3d5f9c19f
Gaddy <gaddy@Gaddys-MacBook-Air.local>
2026-02-04 23:10:30 +0200

    handlers refactoring files heavy

--- handlers/unknown_handler.py
+++
@@ -1,6 +0,0 @@
-
-from sqlalchemy.orm import Session
-from models.inbound_message import InboundMessage
-
-def handle_unknown(db: Session, message: InboundMessage) -> None:
-    pass
--- handlers/workers/registry.py
+++ handlers/workers/registry.py
@@ -1,7 +1,7 @@
 # handlers/work_registry.py
-from handlers.workers.registry import handle_process_inbound
-from handlers.workers.registry import handle_scheduled
-from handlers.workers.registry import handle_scheduled_message
+from handlers.inbound.inbound import handle_process_inbound
+from handlers.effects.scheduled_message_handler import handle_scheduled
+from handlers.effects.tami_scheduled_message import handle_scheduled_message
 
 WORK_HANDLERS = {
     "INBOUND": handle_process_inbound,
